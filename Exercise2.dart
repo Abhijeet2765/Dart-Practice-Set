6dd6a0434a7e3fac9374b6eb0ba387efe9f0c02e
Abhijeet2765 <abhijeetdeshmukh.2765@gmail.com>
2025-01-02 23:53:04 +0530

    Exercise2 Solved

--- Exercise2.dart
+++ Exercise2.dart
@@ -1,1 +1,12 @@
+/*Ask the user for a number. Depending on whether the number is even or odd, print out an appropriate message to the user.*/
+import 'dart:io';
 
+void main() {
+  print("Enter any No: ");
+  int? no = int.parse(stdin.readLineSync() ?? '0');
+  if (no % 2 == 0) {
+    print("Given Number is Even ");
+  } else {
+    print('Given Number is Odd');
+  }
+}
